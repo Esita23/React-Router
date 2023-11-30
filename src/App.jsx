d86aed6974a2d07cd1add182cefd11c08025d2ee
eugene <eugenesitaa@gmail.com>
2023-11-30 12:53:15 +0100

    configutation routes et affichage des produits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import About from './pages/about'
+import ProductLists from './pages/productList'
+import Products from './pages/products'
+import SingleProduct from './pages/singleProduct'
+import NavBar from './components/navBar'
 function App() {
   
 
   return (
-    <>
-     <h1>Hello</h1>
-    </>
+  <BrowserRouter>
+  <NavBar />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/products' element={<Products/>}>
+          <Route index element={<ProductLists />}/>
+          <Route path=':id' element={<SingleProduct/>} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
+
+   
   )
 }
 
